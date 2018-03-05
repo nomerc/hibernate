7c2a1704972b48d4f0c3881024b2755fb75bd6f7
nomerc <nomerc@gmail.com>
2018-03-05 18:24:37 +0300

    ///

--- Hibernate!/HibernateCore.cs
+++ Hibernate!/HibernateCore.cs
@@ -5,7 +5,6 @@ using System.Threading;
 using System.Windows.Controls.Primitives;
 using System.Windows.Threading;
 
-//
 namespace Hibernate_
 {
     class HibernateCore : DependencyObject, INotifyPropertyChanged
